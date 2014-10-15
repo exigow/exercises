32b1a95f93e0fa8fd13203f784b688fbac64fe8c
exigow <jakubblach@gmail.com>
2014-10-15 22:56:06 +0200

    description move

--- java-visitor-root-chooser/src/Runner.java
+++ java-visitor-root-chooser/src/Runner.java
@@ -1,19 +1,4 @@
-/*
-  Zadanie 1.2
-  Napisz program, który pyta o katalog (na przykład używając JFileChooser'a)
-  a następnie wyświetla pełne nazwy dziesięciu największych plików
-  w całym poddrzewie systemu plików którego korzeniem jest wybrany katalog.
-
-  UWAGA: Program nie może zapamiętywać wszystkich napotkanych plików!
-  Znalezione pliki są pamiętane w tablicy o wymiarze 10 uaktualnianej na bieżąco
-  w trakcie przetwarzania drzewa katalogów. [Aby JFileChooser pozwalał wybrać katalog,
-  a nie pojedynczy plik, należy po utworzeniu wywołać
-  na jego rzecz metodę setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
-  i dopiero wtedy wywołać showOpenDialog].
- */
-
 import javax.swing.*;
-import javax.swing.SwingUtilities;
 
 public class Runner {
 
