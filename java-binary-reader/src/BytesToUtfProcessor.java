013a1f6aea0141653cc53ff942081904964d1f6e
exigow <jakubblach@gmail.com>
2014-10-23 09:28:10 +0200

    Added sorting and removing duplicates

--- java-binary-reader/src/BytesToUtfProcessor.java
+++ java-binary-reader/src/BytesToUtfProcessor.java
@@ -4,8 +4,7 @@ class BytesToUtfProcessor {
 
   public final static char NULL_CHAR = '\u0000';
   private final static String UTF_8 = "UTF-8";
-  //private final static Pattern asd = Pattern.compile("\\W*");
-  private final static int[] OFFSETS = new int[]{0x80, 0xe0, 0xf0, 0xf8};
+  private final static int[] MASKS = new int[] {0x80, 0xe0, 0xf0, 0xf8};
   private byte[] utfBuffer = new byte[6];
   private int place = 0;
 
@@ -15,15 +14,6 @@ class BytesToUtfProcessor {
     for (byte aByte : bytes)
       if ((ch = process(aByte)) != NULL_CHAR)
         builder.append(ch);
-    /*String result = builder.toString();
-    String copy = result;
-    Matcher matcher = asd.matcher(result);
-    if (matcher.matches()) {
-      for (int i = 0; i < matcher.groupCount(); i++) {
-        copy = copy.replace(matcher.group(0), "\n");
-      }
-    }
-    return copy;*/
     return builder.toString();
   }
 
@@ -31,19 +21,17 @@ class BytesToUtfProcessor {
     return new String(buffer, 0, place, UTF_8).charAt(0);
   }
 
-
-  private int expected() {
-    int num = utfBuffer[0] & 255;
-    for (int i = 1; i < OFFSETS.length; i++) {
-      if (num < OFFSETS[i])
+  private int getExpectedPlace(byte[] buffer) {
+    int num = buffer[0] & 255;
+    for (int i = 1; i < MASKS.length; i++)
+      if (num < MASKS[i])
         return i;
-    }
     return 5; // last place
   }
 
   private char process(byte nextByte) throws UnsupportedEncodingException {
     utfBuffer[place++] = nextByte;
-    if (place == expected()) {
+    if (place == getExpectedPlace(utfBuffer)) {
       char result = getUtfChar(utfBuffer, place);
       place = 0;
       return result;
--- java-binary-reader/src/Runner.java
+++ java-binary-reader/src/Runner.java
@@ -1,16 +1,21 @@
 import java.io.IOException;
+import java.util.*;
 
 public class Runner {
 
   public static void main(String[] args) throws IOException {
     byte[] bytes = BinaryReader.read("java-binary-reader/test.bin");
     String dirty = new BytesToUtfProcessor().getString(bytes);
-    System.out.println(clearString(dirty));
+    String cleaned = clearString(dirty);
+    Collection<String> strings = Arrays.asList(cleaned.split(" +"));
+    Collections.sort((List)strings);
+    TreeSet<String> treeSet = new TreeSet<String>(strings);
+    for (String str : treeSet)
+      System.out.println(str);
   }
 
   private static String clearString(String source) {
-    return source.replaceAll("\\W*", "");
+    return source.replaceAll("\\W", " ");
   }
-
 
 }
