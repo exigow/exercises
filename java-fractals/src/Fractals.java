d9117695a0a04db164ab00a5eba489184cd76801
exigow <jakubblach@gmail.com>
2014-10-30 18:54:56 +0100

    Filling, orange, ORANGE, OOORRRAAAANGGEEEE@@@@

--- java-fractals/src/Fractals.java
+++ java-fractals/src/Fractals.java
@@ -4,28 +4,33 @@ import javax.swing.*;
 
 public class Fractals extends JFrame {
 
-  private BufferedImage image;
+  private final static Dimension WINDOW_SIZE = new Dimension(640, 480);
+  private BufferedImage generated;
 
   public Fractals() {
     super("fractals");
-    setSize(256, 256);
+    setSize(WINDOW_SIZE);
+    setLocationRelativeTo(null);
     setResizable(false);
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    image = generate(getWidth(), getHeight());
+    generated = generate(getWidth(), getHeight());
   }
 
   private BufferedImage generate(int width, int height) {
     BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    Color color = new Color(255, 255, 255);
-    for (int i = 0; i < width; i++)
+    Graphics2D graphics2D = result.createGraphics();
+    graphics2D.setColor(new Color(255, 128, 64));
+    graphics2D.fillRect(0, 0, result.getWidth(), result.getHeight());
+    //fill(result, new Color(255, 128, 32));
+    /*for (int i = 0; i < width; i++)
       for (int j = 0; j < height; j++)
-        result.setRGB(i, j, color.getRGB());
+        result.setRGB(i, j, color.getRGB());*/
     return result;
   }
 
   @Override
   public void paint(Graphics graphics) {
-    graphics.drawImage(image, 0, 0, this);
+    graphics.drawImage(generated, 0, 0, this);
   }
 
 }
