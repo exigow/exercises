670ca8176d2730f9043de4e808448cf04468aee7
exigow <jakubblach@gmail.com>
2014-10-30 19:22:56 +0100

    Generating colors and custom filling

--- java-fractals/src/Fractals.java
+++ java-fractals/src/Fractals.java
@@ -18,14 +18,33 @@ public class Fractals extends JFrame {
 
   private BufferedImage generate(int width, int height) {
     BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    Graphics2D graphics2D = result.createGraphics();
-    graphics2D.setColor(new Color(255, 128, 64));
-    graphics2D.fillRect(0, 0, result.getWidth(), result.getHeight());
-    //fill(result, new Color(255, 128, 32));
-    /*for (int i = 0; i < width; i++)
-      for (int j = 0; j < height; j++)
-        result.setRGB(i, j, color.getRGB());*/
+    fill(result, new Color(255, 0, 255));
+    float seedX = .5f, seedY = .175f;
+    for (int ix = 0; ix < width; ix++)
+      for (int iy = 0; iy < height; iy++)
+        for (int i = 0; i < 32; i++) {
+          float
+            x = (float) ix / width,
+            y = (float) iy / height;
+          result.setRGB(ix, iy, generateRgb(x, y, 0f));
+        }
     return result;
+  }
+
+  /*
+    f <0; 1>
+  */
+  private int generateRgb(float r, float g, float b) {
+    return
+      (((int)(r * 255) & 0x0ff) << 16) |
+      (((int)(g * 255) & 0x0ff) << 8) |
+      ((int)(b * 255) & 0x0ff);
+  }
+
+  private static void fill(BufferedImage image, Color color) {
+    Graphics2D g2d = image.createGraphics();
+    g2d.setColor(color);
+    g2d.fillRect(0, 0, image.getWidth(), image.getHeight());
   }
 
   @Override
