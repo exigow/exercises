52735e1da582c913d46209c2953c2d9cb10ec11c
exigow <jakubblach@gmail.com>
2014-10-30 19:43:31 +0100

    Its working, now im seeing fractals!

--- java-fractals/src/Fractals.java
+++ java-fractals/src/Fractals.java
@@ -1,6 +1,6 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import javax.swing.*;
 
 public class Fractals extends JFrame {
 
@@ -18,33 +18,32 @@ public class Fractals extends JFrame {
 
   private BufferedImage generate(int width, int height) {
     BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    fill(result, new Color(255, 0, 255));
-    float seedX = .5f, seedY = .175f;
+    float cx = .5f, cy = .5f;
     for (int ix = 0; ix < width; ix++)
-      for (int iy = 0; iy < height; iy++)
-        for (int i = 0; i < 32; i++) {
+      for (int iy = 0; iy < height; iy++) {
+        float
+          zx = ((float) ix / width - .5f) * 4f,
+          zy = ((float) iy / height - .5f) * 4f;
+        int i = 0;
+        for (; i < 512; i++) {
           float
-            x = (float) ix / width,
-            y = (float) iy / height;
-          result.setRGB(ix, iy, generateRgb(x, y, 0f));
+            x = (zx * zx - zy * zy) + cx,
+            y = (zy * zx + zx * zy) + cy;
+          if ((zx * zx + zy * zy) > 4.0f)
+            break;
+          zx = x;
+          zy = y;
         }
+        result.setRGB(ix, iy, generateRgb((float) i / 16f, 0f, 0f));
+      }
     return result;
   }
 
-  /*
-    f <0; 1>
-  */
   private int generateRgb(float r, float g, float b) {
     return
       (((int)(r * 255) & 0x0ff) << 16) |
       (((int)(g * 255) & 0x0ff) << 8) |
       ((int)(b * 255) & 0x0ff);
-  }
-
-  private static void fill(BufferedImage image, Color color) {
-    Graphics2D g2d = image.createGraphics();
-    g2d.setColor(color);
-    g2d.fillRect(0, 0, image.getWidth(), image.getHeight());
   }
 
   @Override
