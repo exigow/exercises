9ab1ec1783df5cf6654ea717d48b8d9e3984a23a
Jakub Błach <jakubblach@gmail.com>
2014-11-07 10:46:44 +0100

    wip

--- monotonic-sequence/main.cpp
+++ monotonic-sequence/main.cpp
@@ -8,14 +8,44 @@ inline int next() {
   while (c >= 48 && c <= 57)
     n = n * 10 + c - 48,
       c = getchar_unlocked();
+  printf("next is %d\n", n);
   return n;
 }
 
+struct Series {
+  int counter,
+    sum;
+};
+
+void print_series(Series s) {
+  printf("series: counter: %d, sum: %d\n", s.counter, s.sum);
+}
+
 int main() {
-  int x = next();
-  while (x != -1) {
-    printf("%d ", x);
-    x = next();
+  int value,
+    prev = 0;
+  Series actual = Series(),
+    maximum = Series();
+  while (true) {
+    value = next();
+    if (value == -1)
+      break;
+    if (value >= prev) {
+      printf("value %d is bigger or equal than %d\n", value, prev);
+      prev = value;
+      actual.sum += value;
+      actual.counter++;
+      print_series(actual);
+    }
+    else {
+      /*if (actual.counter >= maximum.counter)
+        maximum = actual;
+      actual.counter = 1;
+      actual.sum = value;*/
+    }
   }
+  print_series(actual);
   return 0;
 }
+
+
