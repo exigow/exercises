ecc72073ada7d371d4bf30f4172952d7f24c1a08
Jakub Błach <jakubblach@gmail.com>
2014-11-16 19:45:55 +0100

    runners

--- java-network-multiplayer/src/client/ClientRunner.java
+++ java-network-multiplayer/src/client/ClientRunner.java
@@ -1,17 +1,15 @@
 package client;
 
 import dialogs.IpAddressDialog;
-import dialogs.NickDialog;
 import dialogs.PortDialog;
 
 public class ClientRunner {
 
   public static void main(String[] args) throws Exception {
-    String nick = new NickDialog().execute();
     String ip = new IpAddressDialog().execute();
     String port = new PortDialog().execute();
     int portValue = Integer.parseInt(port);
-    new Client(nick, ip, portValue).execute();
+    new ChatClient(ip, portValue).run();
   }
 
 }
