daa774221a5cce73ec38bad7caebd73e2463de53
Jakub Błach <jakubblach@gmail.com>
2014-12-08 21:19:02 +0100

    working???

--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -9,9 +9,9 @@ struct Node {
 };
 
 inline void printNode(Node *node) {
+  printf("%d\n", node->value);
   if (node->left)
     printNode(node->left);
-  printf("%d\n",node->value);
   if(node->right)
     printNode(node->right);
 }
@@ -19,22 +19,37 @@ inline void printNode(Node *node) {
 int main() {
   int c;
   bool rooting = true;
-  Node *root = NULL;
+  Node *root = NULL, *walker;
   do {
-    Node *created = new Node(), *walker = NULL;
-    created->value = getchar_unlocked();
+    Node *created = new Node();
+    int value = getchar_unlocked();
+    created->left = NULL;
+    created->right = NULL;
     if (rooting)
       rooting = false,
-      root = created;
+      root = created,
+      root->value = value;
     getchar_unlocked(); // skip space
+    walker = root;
+    cout << "walker starting from " << walker->value << endl;
     while (true) {
       c = getchar_unlocked();
       if (c == EOF | c == 10) {
         cout << endl;
         break;
       }
+      if (c == 'L') {
+        if (walker->left == NULL)
+          walker->left = new Node();
+        walker = walker->left;
+      } else {
+        if (walker->right == NULL)
+          walker->right = new Node();
+        walker = walker->right;
+      }
       cout << (char) c;
     }
+    walker->value = value;
   } while (c != EOF);
   printNode(root);
   return 0;
