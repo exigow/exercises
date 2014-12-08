c857e2a3f6a427acd832b6f3a13903bfd02ebf2d
Jakub Błach <jakubblach@gmail.com>
2014-12-08 21:06:46 +0100

    root node

--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
 #include <stdio.h>
 
-struct node {
-  char value;
-  struct node *right, *left;
+using namespace std;
+
+struct Node {
+  int value;
+  struct Node *right, *left;
 };
 
-void printout(node *tree) {
-  if (tree->left)
-    printout(tree->left);
-  printf("%d\n", tree->value);
-  if(tree->right)
-    printout(tree->right);
-}
-
-void insert(node ** tree, node * item) {
-  if(!(*tree)) {
-    *tree = item;
-    return;
-  }
-  if (item->value < (*tree)->value)
-    insert(&(*tree)->left, item);
-  else if (item->value>(*tree)->value)
-    insert(&(*tree)->right, item);
+inline void printNode(Node *node) {
+  if (node->left)
+    printNode(node->left);
+  printf("%d\n",node->value);
+  if(node->right)
+    printNode(node->right);
 }
 
 int main() {
-  node *curr, *root = NULL;
-
   int c;
+  bool rooting = true;
+  Node *root = NULL;
   do {
-    int letter = getchar_unlocked();
-    std::cout << letter << " --> ";
+    Node *created = new Node(), *walker = NULL;
+    created->value = getchar_unlocked();
+    if (rooting)
+      rooting = false,
+      root = created;
     getchar_unlocked(); // skip space
     while (true) {
       c = getchar_unlocked();
       if (c == EOF | c == 10) {
-        std::cout << std::endl;
+        cout << endl;
         break;
       }
-      std::cout << c << ", ";
+      cout << (char) c;
     }
   } while (c != EOF);
+  printNode(root);
   return 0;
 }
