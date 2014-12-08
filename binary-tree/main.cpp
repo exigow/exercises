81d4b1423f4979e8eb618a3c159f1e854065b426
Jakub Błach <jakubblach@gmail.com>
2014-12-08 20:47:47 +0100

    test

--- binary-tree/main.cpp
+++ binary-tree/main.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
 #include <stdio.h>
 
+struct node {
+  char value;
+  struct node *right, *left;
+};
+
+void printout(node *tree) {
+  if (tree->left)
+    printout(tree->left);
+  printf("%d\n", tree->value);
+  if(tree->right)
+    printout(tree->right);
+}
+
+void insert(node ** tree, node * item) {
+  if(!(*tree)) {
+    *tree = item;
+    return;
+  }
+  if (item->value < (*tree)->value)
+    insert(&(*tree)->left, item);
+  else if (item->value>(*tree)->value)
+    insert(&(*tree)->right, item);
+}
+
 int main() {
+  node *curr, *root = NULL;
+
   int c;
   do {
-    c = getchar_unlocked();
-    std::cout << c << " --> ";
+    int letter = getchar_unlocked();
+    std::cout << letter << " --> ";
     getchar_unlocked(); // skip space
     while (true) {
       c = getchar_unlocked();
