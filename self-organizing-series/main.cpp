5f43a65638a215a5e9b6c30ae7382232062ab356
Jakub Błach <jakubblach@gmail.com>
2015-04-18 11:19:22 +0200

    recur list

--- self-organizing-series/main.cpp
+++ self-organizing-series/main.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
 
-
-struct entity {
-  entity* left, right;
-};
-
 using namespace std;
 
+inline long next() {
+  register long n = 0;
+  register int c = getchar_unlocked();
+  if (c == -1)
+    return -1;
+  while (c > 47 && c < 58)
+    n = (n << 1) + (n << 3) + c - 48,
+      c = getchar_unlocked();
+  return n;
+}
+
+struct Node {
+  long value;
+  Node *next;
+};
+
+static void printRecursive(Node *n) {
+  if (n == nullptr)
+    printf("null");
+  else {
+    printf("(%lu)->", n->value);
+    printRecursive(n->next);
+  }
+}
+
 int main() {
-  cout << "Hello, World!" << endl;
+  long count = next();
+  Node *prev = nullptr;
+  Node *head = nullptr;
+  bool noBlock = true;
+  long readedValue = next();
+  do {
+    Node *node = new Node();
+    node->value = readedValue;
+    if (prev != nullptr)
+      prev->next = node;
+    prev = node;
+    readedValue = next();
+    if (noBlock)
+      head = node, noBlock = false;
+  } while (readedValue != EOF);
+  printRecursive(head);
   return 0;
 }
