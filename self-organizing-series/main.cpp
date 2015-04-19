4069d1f559ab997fe4a58d4003e658364a7fb9e9
Jakub Błach <jakubblach@gmail.com>
2015-04-19 15:57:18 +0200

    skipowanie w przypadku kiedy ilosc krokow jest wieksza niz ilosc elm w ciagu

--- self-organizing-series/main.cpp
+++ self-organizing-series/main.cpp
@@ -3,24 +3,43 @@
 
 using namespace std;
 
-inline long next() {
-  register long n = 0;
+typedef unsigned int uint;
+
+long size = 0;
+
+inline int fetchNext() {
+  register int n = 0;
   register int c = getchar_unlocked();
-  if (c == -1)
-    return -1;
+  if (c == EOF)
+    return EOF;
   while (c > 47 && c < 58)
     n = (n << 1) + (n << 3) + c - 48,
-      c = getchar_unlocked();
+    c = getchar_unlocked();
   return n;
 }
 
+inline void putNext(uint n) {
+  if (n == 0) {
+    putchar_unlocked(48);
+    return;
+  }
+  char tab[12];
+  int p = 0;
+  while (n != 0) {
+    tab[p++] = (char) ((n % 10) + 48);
+    n /= 10;
+  }
+  while (p--)
+    putchar_unlocked(tab[p]);
+}
+
 struct Node {
-  long value;
+  uint value;
   Node *next;
 };
 
 inline void printNode(Node *node) {
-  printf("%lu ", node->value);
+  putNext(node->value);
 }
 
 inline void printIterative(Node *node) {
@@ -29,21 +48,25 @@ inline void printIterative(Node *node) {
   do {
     printNode(temp);
     temp = temp->next;
-  } while (temp != start);
+    if (temp == start)
+      break;
+    putchar_unlocked(' ');
+  } while (true);
 }
 
 inline Node *loadAndGetHead() {
   Node *last = NULL;
   Node *head = NULL;
   bool noBlock = true;
-  long readedValue = next();
+  int readedValue = fetchNext();
   do {
     Node *node = new Node();
-    node->value = readedValue;
+    size++;
+    node->value = (uint) readedValue;
     if (last != NULL)
       last->next = node;
     last = node;
-    readedValue = next();
+    readedValue = fetchNext();
     if (noBlock)
       head = node, noBlock = false;
   } while (readedValue != EOF);
@@ -51,46 +74,50 @@ inline Node *loadAndGetHead() {
   return head;
 }
 
-inline long removeNext(Node *node) {
+inline uint removeNext(Node *node) {
   Node *forward = node->next->next;
-  long val = node->next->value;
+  uint val = node->next->value;
   delete node->next;
+  size--;
   node->next = forward;
   return val;
 }
 
-inline long addNext(Node *node) {
+inline uint addNext(Node *node) {
   Node *temp = node->next;
   Node *created = new Node();
-  created->value = node->value - 1;
+  size++;
+  uint value = node->value;
+  created->value = value - 1;
   node->next = created;
   created->next = temp;
-  return node->value;
+  return value;
 }
 
-inline Node *walk(Node* from, long steps) {
+inline Node *walk(Node* from, uint steps) {
   Node *temp = from;
-  for (long i = 0; i < steps; i++)
+  for (uint i = 0; i < steps; i++)
     temp = temp->next;
   return temp;
-  // todo skip jesli jest steps >= list.size
 }
 
 inline bool isEven(Node *node) {
   return node->value % 2 == 0;
 }
 
-inline long hehe(Node *pivot) {
+inline uint performAction(Node *pivot) {
   if (isEven(pivot))
     return removeNext(pivot);
   return addNext(pivot);
 }
 
 int main() {
-  long count = next();
+  int count = fetchNext();
   Node *pivot = loadAndGetHead();
-  for (int i = 0; i < count; i++) {
-    long steps = hehe(pivot);
+  for (uint i = 0; i < count; i++) {
+    uint steps = performAction(pivot);
+    if (steps >= size)
+      steps -= size;
     pivot = walk(pivot, steps);
   }
   printIterative(pivot);
