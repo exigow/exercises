f18fcd97bbfd9088f49b554410311d2078da790f
Jakub Błach <jakubblach@gmail.com>
2015-05-10 16:00:28 +0200

    działa, ale nie jest czysty kod

--- lexicographical-tree/main.cpp
+++ lexicographical-tree/main.cpp
@@ -42,22 +42,63 @@ inline static Node *buildTree() {
   return root;
 }
 
-inline char toChar(Node *node) {
-  if (node == NULL)
-    return '_';
-  return (char) node->letter;
-}
-
-inline void printNode(Node *node) {
+inline static void printNode(Node *node) {
   if (node == NULL)
     return;
-  printf("node %c with left: %c, right %c\n", toChar(node), toChar(node->left), toChar(node->right));
   printNode(node->left);
   printNode(node->right);
+  printf("%c", node->letter);
+}
+
+inline static bool isOnlyOneLeaf(Node *node) {
+  if (node->left && node->right)
+    return false;
+  if (node->left)
+    return isOnlyOneLeaf(node->left);
+  if (node->right)
+    return isOnlyOneLeaf(node->right);
+  return true;
+}
+
+struct Result {
+  int left = 0;
+  int right = 0;
+};
+
+inline static int reduce(Node *element) {
+  int tempLeft = 0;
+  int tempRight = 0;
+  if (!element->left && !element->right)
+    return element->letter - 70;
+  else {
+    if (element->left)
+      tempLeft = reduce(element->left);
+    if (element->right)
+      tempRight = reduce(element->right);
+  }
+  if (tempLeft != 0 && tempLeft < 90) {
+    element->left = NULL;
+    tempLeft = tempLeft + 70;
+  }
+  if (tempRight != 0 && tempRight < 90) {
+    element->right = NULL;
+    tempRight = tempRight + 70;
+  }
+  if (tempLeft > tempRight) {
+    element->right = NULL;
+    return tempLeft;
+  }
+  if (tempLeft < tempRight) {
+    element->left = NULL;
+    return tempRight;
+  }
+  return tempRight;
 }
 
 int main() {
   Node *root = buildTree();
+  while (!isOnlyOneLeaf(root))
+    printf("%c", reduce(root));
   printNode(root);
   return 0;
 }
